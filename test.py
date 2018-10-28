ae20bfd38f26c168bd1d768f4103f7d6556d9426
Akhila Veerapuraju <dhanya.akhila@iiitb.org>
2018-10-28 19:59:09 +0530

    Added train, test and required files

---
+++ test.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+import os
+import pickle
+
+
+
+mfcc_test_path = "./features/mfcc/test/timit.hdf"
+mfcc_delta_test_path = "./features/mfcc_delta/test/timit.hdf"
+mfcc_delta_delta_test_path = "./features/mfcc_delta_delta/test/timit.hdf"
+
+dump_file_path = "./Models"
+
+delta_list = [0, 1, 2]
+test_path_list = [mfcc_test_path, mfcc_delta_test_path, mfcc_delta_delta_test_path]
+
+
+
+def main(delta, energy_coeff, models_path):
+
+    test_features, test_labels = extracting_features_labels(delta, energy_coeff)
+    models_dict = load_models(models_path)
+
+    fer = testing(test_features, test_labels, models_dict)
+    print(models_path, " ===== ", fer)
+
+
+
+def extracting_features_labels(delta, energy_coeff):
+
+    df = pd.read_hdf(test_path_list[int(delta)])
+
+    features = np.array(df["features"].tolist())
+    labels = np.array(df["labels"].tolist())
+
+
+    if(energy_coeff is False):
+
+        if (delta == '0'):
+            features = np.delete(features, 0, axis=1)
+
+        if (delta == '1'):
+            features = np.delete(features, [0, 13], axis=1)
+
+        if (delta == '2'):
+            features = np.delete(features, [0, 13, 26], axis=1)
+
+    return features, labels
+
+
+
+def load_models(path):
+
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+    return model
+
+
+
+def testing(test_features, test_labels, models_dict):
+
+    scores = []
+
+    for label, model in models_dict.items():
+        scores.append(model.score_samples(test_features))
+
+    scores = np.array(scores)
+    pred_indices = scores.argmax(axis = 0)
+
+    pred_labels = []
+    labels_list = list(models_dict.keys())
+
+    for idx in pred_indices:
+        pred_labels.append(labels_list[idx])
+
+    return get_accuracy(test_labels, np.array(pred_labels))
+
+
+
+def get_accuracy(test_labels, pred_labels):
+
+    common_list = [i for i, j in zip(test_labels, pred_labels) if i == j]
+    cmp_perct = (len(common_list) / len(pred_labels)) * 100
+
+    return cmp_perct
+
+
+
+if __name__ == '__main__':
+
+    for dirName, subdirList, fileList in os.walk(dump_file_path):
+
+        for mName in fileList:
+
+            if(mName[0] != '.'):
+
+                split = mName.split("_")
+
+                delta = split[0]
+                energy_coeff = True
+
+                if (split[-1] == 'noEC.pkl'):
+                    energy_coeff = False
+
+                main(delta, energy_coeff, dirName+ "/"+mName)
---
+++ train.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.mixture import GaussianMixture
+
+
+
+mfcc_train_path = "./features/mfcc/train/timit.hdf"
+mfcc_delta_train_path = "./features/mfcc_delta/train/timit.hdf"
+mfcc_delta_delta_train_path = "./features/mfcc_delta_delta/train/timit.hdf"
+
+delta_list = [0, 1, 2]
+train_path_list = [mfcc_train_path, mfcc_delta_train_path, mfcc_delta_delta_train_path]
+
+dump_file_path = "./Models"
+
+
+
+def main(caseID, delta, energy_coeff, num_mixtures):
+
+    training_dict = constructing_training_dict(delta, energy_coeff)
+
+    models_dict = training(training_dict, num_mixtures)
+
+    dump_models(caseID, models_dict, delta, energy_coeff, num_mixtures)
+
+
+
+def constructing_training_dict(delta, energy_coeff):
+
+    df = pd.read_hdf(train_path_list[delta])
+
+    features = np.array(df["features"].tolist())
+    labels = np.array(df["labels"].tolist())
+
+
+    if(energy_coeff is False):
+
+        if(delta == 0):
+            features = np.delete(features, 0, axis = 1)
+
+        if(delta == 1):
+            features = np.delete(features, [0, 13], axis = 1)
+
+        if(delta == 2):
+            features = np.delete(features, [0, 13 , 26], axis = 1)
+
+
+    unique_labels = np.unique(labels)
+    training_dict = {key: [] for key in unique_labels}
+
+    for label, feature in zip(labels, features):
+        training_dict[label].append(feature)
+
+    for label, features in training_dict.items():
+        training_dict[label] = np.array(training_dict[label])
+
+    return training_dict
+
+
+
+def training(training_dict, num_mixtures):
+
+    models_dict = {}
+
+    for label, features in training_dict.items():
+        model = GaussianMixture(n_components = int(num_mixtures),
+                                covariance_type = "diag")
+        model.fit(features)
+        models_dict[label] = model
+
+    return models_dict
+
+
+
+def dump_models(caseID, models_dict, delta, energy_coeff, num_mixtures):
+
+    path =  "/Case " + caseID + "/" + str(delta) + "_" + num_mixtures+ "_"
+
+    if(energy_coeff is True):
+        path = path + "EC"
+    else:
+        path = path + "noEC"
+
+    path = dump_file_path + path + ".pkl"
+
+    with open(path, 'wb') as f:
+        pickle.dump(models_dict, f)
+
+
+
+if __name__ == '__main__':
+
+    for delta in delta_list:
+        main('1', delta, True, '064')
+
+    for delta in delta_list:
+        main('2', delta, False, '064')
+
+
+    num_mixtures_list = ['002', '004', '008', '016', '032', '064', '128', '256']
+
+    for num_mixtures in num_mixtures_list:
+        main('3', 0, False, num_mixtures)
+
+
+
+
+
